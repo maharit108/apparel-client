f68b796998d1e85ee44a7e79a072d7b5bb593a88
maharit108 <maharit108@gmail.com>
2020-11-01 11:50:30 -0600

    Add server path

--- src/apiConfig.js
+++ src/apiConfig.js
@@ -1,6 +1,6 @@
 let apiUrl
 const apiUrls = {
-  production: 'https://aqueous-atoll-85096.herokuapp.com',
+  production: 'https://git.heroku.com/whispering-hollows-83607.git',
   development: 'http://localhost:4741'
 }
 
